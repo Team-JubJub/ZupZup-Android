2e8cad39502ad3e2073a07aa66ce9511dfdf005d
Jiwoo <a980917a@naver.com>
2022-11-22 22:04:05 +0900

    [Chore] 중간 커밋

---
+++ app/src/main/java/com/example/zupzup/ui/fragment/ReservationProcessFragment.kt
@@ -0,0 +1,31 @@
+package com.example.zupzup.ui.fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.example.zupzup.databinding.FragmentReservationProcessBinding
+import com.example.zupzup.ui.viewmodel.ReservationViewModel
+
+class ReservationProcessFragment : Fragment() {
+
+    private var _binding: FragmentReservationProcessBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentReservationProcessBinding.inflate(layoutInflater, container, false)
+        return _binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
