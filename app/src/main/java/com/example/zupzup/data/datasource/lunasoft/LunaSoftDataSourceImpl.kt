55d9a09cad91f17829d666ef5d4c2e4f1a736c5d
Jiwoo <a980917a@naver.com>
2022-12-18 00:22:10 +0900

    [Chore] 불필요한 context 제거 및 textSize 수정

--- app/src/main/java/com/example/zupzup/data/datasource/lunasoft/LunaSoftDataSourceImpl.kt
+++ app/src/main/java/com/example/zupzup/data/datasource/lunasoft/LunaSoftDataSourceImpl.kt
@@ -1,7 +1,6 @@
 package com.example.zupzup.data.datasource.lunasoft
 
 
-import android.content.Context
 import com.example.zupzup.data.dto.lunasoft.parameter.LunaSoftRequestBody
 import com.example.zupzup.data.dto.lunasoft.parameter.Message
 import com.example.zupzup.data.dto.lunasoft.response.LunaSoftResponse
@@ -9,25 +8,23 @@ import com.example.zupzup.data.service.LunaSoftService
 import com.example.zupzup.utils.Constants.apiKey
 import com.example.zupzup.utils.Constants.defaultTemplateId
 import com.example.zupzup.utils.Constants.userId
-import dagger.hilt.android.qualifiers.ApplicationContext
 import retrofit2.Response
 import javax.inject.Inject
 
 class LunaSoftDataSourceImpl @Inject constructor(
     private val lunaSoftService: LunaSoftService,
-    @ApplicationContext private val context: Context
 ) : LunaSoftDataSource {
 
     override suspend fun sendNotificationTalk(messages: List<Message>): Response<LunaSoftResponse> {
-//        val body = LunaSoftRequestBody(
-//            userId = userId,
-//            apiKey = apiKey,
-//            templateId = defaultTemplateId,
-//            messages = messages
-//        )
-//        return lunaSoftService.sendNotificationTalk(
-//            parameter = body
-//        )
-        return Response.success(LunaSoftResponse(0,null))
+        val body = LunaSoftRequestBody(
+            userId = userId,
+            apiKey = apiKey,
+            templateId = defaultTemplateId,
+            messages = messages
+        )
+        return lunaSoftService.sendNotificationTalk(
+            parameter = body
+        )
+        //return Response.success(LunaSoftResponse(0,null))
     }
 }
